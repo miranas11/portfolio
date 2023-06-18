1794dc32bf499f9396d1855f869569a486687e7e
miranas11 <anasmir24@gmail.com>
2023-06-19 04:17:39 +0530

    following changes- made the structure of App defined some constants and util classes in css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+//
+//
+//
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div className="container section">
+                <h1>
+                    lorem ipufa f afkajhfas fhkja fkashfhdksa fkasfhak
+                    fkaskfhkasfkhak fkahfkahkfhk gfhkdghfkdhkier ie d
+                </h1>
+                <button className="btn">click</button>
+            </div>
+            <div className="container section">
+                <h1>
+                    lorem ipufa f afkajhfas fhkja fkashfhdksa fkasfhak
+                    fkaskfhkasfkhak fkahfkahkfhk gfhkdghfkdhkier ie d
+                </h1>
+                <button className="btn">click</button>
+            </div>
+        </>
+    );
 }
 
 export default App;
