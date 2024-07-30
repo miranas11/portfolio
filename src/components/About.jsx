39b223058e9f40f3e10f2893872eee12d5f30946
miranas11 <anasmir24@gmail.com>
2024-07-31 02:35:59 +0400

    a

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,19 +7,15 @@ const About = () => {
             <div className="about__content">
                 <h2 className="about__title">About</h2>
                 <p className="about__description">
-                    Hi! I am Mir Anas, a Full-Stack Web Developer and Mobile App
-                    Developer.
+                    Hi! I am Mir Anas, a Full-Stack Web Developer with 2 years
+                    of experience
                 </p>
                 <p className="about__description">
-                    I have completed an intership at Magic EdTech as a Backend
-                    Developer where i was tasked to build REST API's using
-                    Spring Boot. After completing my intership i started
-                    exploring the MERN Stack and i have built several projects
-                    while learning the same.
-                </p>
-                <p className="about__description">
-                    I have completed my Bachelors Degree in Computer Science
-                    from Sharda University in 2023.
+                    I am a Software Development Engineer with a Bachelor of
+                    Technology in Computer Science from Sharda University. My
+                    expertise spans front-end and back-end development,
+                    highlighted by a strong command of Node.js, ReactJs, and
+                    JavaScript.
                 </p>
                 <hr className="about__hr" />
                 <h3 className="about__subtitle">Technologies</h3>
@@ -27,8 +23,8 @@ const About = () => {
                     <ul className="about__ul">
                         <li className="about__listitems">JavaScript</li>
                         <li className="about__listitems">React.JS</li>
+                        <li className="about__listitems">Node.JS</li>
                         <li className="about__listitems">Express</li>
-                        <li className="about__listitems">Flutter</li>
                     </ul>
                     <ul className="about__ul">
                         <li className="about__listitems">MongoDB</li>
